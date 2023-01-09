2c626f106870ac56f35ef83b430ab54630e5df0a
Denis <denissdv03@gmail.com>
2023-01-09 17:41:24 +0200

    Створені функції. Додано перерахування "enum RowState". Створено об'єкт DB. Створена функція "Створення стовбців таблиці". Створена функція "Надання типів даних для полів". Створена функція "Зчитування даних з бази даних".

--- ZooLab5/AdminForm/Main_Form_Admin.cs
+++ ZooLab5/AdminForm/Main_Form_Admin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,17 @@ using System.Windows.Forms;
 
 namespace ZooLab5
 {
+    enum RowState
+    {
+        Existed,
+        New,
+        Modified,
+        ModifiedNew,
+        Deleted
+    }
     public partial class Main_Form_Admin : Form
     {
+        DataBase DB = new DataBase();
         public Main_Form_Admin()
         {
             InitializeComponent();
@@ -19,7 +29,52 @@ namespace ZooLab5
 
         private void Main_Form_Admin_Load(object sender, EventArgs e)
         {
+            CreateColumns();
+            RefreshDataGrid(dataGridView1);
+        }
 
+        /// <summary>
+        /// Створення стовбців таблиці
+        /// </summary>
+        private void CreateColumns()
+        {
+            dataGridView1.Columns.Add("Id", "Id");
+            dataGridView1.Columns.Add("Password", "Password");
+            dataGridView1.Columns.Add("Login", "Login");
+            dataGridView1.Columns.Add("IsNew", string.Empty);
+        }
+
+        /// <summary>
+        /// Надання типів даних для полів.
+        /// </summary>
+        /// <param name="dgw"></param>
+        /// <param name="record"></param>
+        private void ReadSingleRows(DataGridView dgw, IDataReader record)
+        {
+            dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), RowState.ModifiedNew);
+        }
+
+        /// <summary>
+        /// Зчитування даних з бази даних.
+        /// </summary>
+        /// <param name="dgw"></param>
+        private void RefreshDataGrid(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+
+            string queryString = $"SELECT * FROM Users";
+            SqlCommand command = new SqlCommand(queryString, DB.getConnection());
+
+            DB.openConnection();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                ReadSingleRows(dgw, reader);
+            }
+            reader.Close();
+            DB.closeConnection();
         }
 
         private void Main_Form_Admin_FormClosed(object sender, FormClosedEventArgs e)
